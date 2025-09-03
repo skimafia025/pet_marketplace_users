af4ad640514c27cb430c612ff810823c8c8a8b57
a.kasmaliev <a.kasmaliev@rsktech.kg>
2025-09-03 18:38:41 +0600

    feat: Add docker-compose.yml Dockerfile and settings

--- config/database.go
+++ config/database.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"pet_marketplace_users/models"
@@ -16,11 +17,19 @@ var DB *gorm.DB
 func ConnectDB() {
 	godotenv.Load()
 
-	dsn := os.Getenv("DATABASE_URL")
+	// Получаем параметры из переменных окружения
+	dbHost := getEnv("DB_HOST", "localhost")
+	dbPort := getEnv("DB_PORT", "5432")
+	dbUser := getEnv("DB_USER", "postgres")
+	dbPassword := getEnv("DB_PASSWORD", "password")
+	dbName := getEnv("DB_NAME", "pet_marketplace_users")
 
-	if dsn == "" {
-		dsn = "host=localhost user=postgres password=password dbname=pet_marketplace_users port port5432 sslmode=disable"
-	}
+	// Формируем DSN из переменных
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		dbHost, dbUser, dbPassword, dbName, dbPort)
+
+	log.Printf("Подключаемся к БД: %s:%s", dbHost, dbPort)
+
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
@@ -30,6 +39,14 @@ func ConnectDB() {
 		log.Fatal("Не удалось подключиться к БД:", err)
 	}
 
+	log.Printf("Успешно подключились к БД!")
 	DB.AutoMigrate(&models.User{})
+}
 
+// Вспомогательная функция для получения переменных окружения
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
 }
